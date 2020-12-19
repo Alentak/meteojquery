c3d3bf00e6c9cd9c47a1cdeeda2627b88a87e668
Paul Guillon <alentak27@gmail.com>
2020-12-19 22:49:27 +0100

    card, current page, navsearch, icons, flag, url, unknown city

---
+++ js/index.js
@@ -0,0 +1,17 @@
+//Main Form
+document.querySelector('#searchForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let searchValue = document.querySelector('#search').value;
+
+    window.location.href = `./current.html?city=${searchValue}`;
+});
+
+//Nav form
+document.querySelector('#navSearchForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    let navSearchValue = document.querySelector('#navSearch').value;
+
+    window.location.href = `./current.html?city=${navSearchValue}`;
+});
