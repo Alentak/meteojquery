6560999f8efabbd648408e58a3133e9ba1475174
Paul Guillon <alentak27@gmail.com>
2020-12-31 21:51:45 +0100

    forecast

--- js/forecast.js
+++ js/forecast.js
@@ -12,39 +12,36 @@ fetch(apiUrl).then(function (response) {
             jours[data.dt_txt.substr(0, 10)] = [data];
         }
     });
-    document.write(`<h1 style="font-family: 'Operator Mono Lig Light';">À beuzeville</h1>`);
+    let container = document.querySelector('main');
+    let textHtml = '';
+
+    textHtml = `<h1 style="font-family: 'Operator Mono Lig Light';">À beuzeville</h1>`
     
-    document.write(`<table cellpadding="5" style="font-family: 'Operator Mono Lig Light';border:1px solid black;text-align:center;background-color:lightgray;">`);
-        document.write(`<thead style="background-color:gray;color:white">`);
-            document.write(`<tr>`);
-                document.write(`<th>`);
-                    document.write(`Jour`);
-                document.write(`</th>`);
-                document.write(`<th colspan="8">`);
-                    document.write(`Horaire`);
-                document.write(`</th>`);
-            document.write(`</tr>`);
-        document.write(`</thead>`);
-        document.write(`<tbody style="background-color:white;">`);
+    textHtml += `<table cellpadding="5" style="font-family: 'Operator Mono Lig Light';text-align:center;background-color:lightgray;">`
+        textHtml += `<thead style="background-color:gray;color:white">`
+            textHtml += `<tr>`
+                textHtml += `<th>Jour</th>`
+                textHtml += `<th colspan="8">Horaire</th>`
+            textHtml += `</tr>`
+        textHtml += `</thead>`
+        textHtml += `<tbody style="background-color:white;">`
         for (const jour in jours) {
-            document.write(`<tr>`);
-                document.write(`<td>`);
-                document.write(new Date(jour).toString().substr(0, 10));
-                document.write(`</td>`);
+            textHtml += `<tr>`
+                textHtml += `<td>`
+                    textHtml += new Date(jour).toString().substr(0, 10)
+                textHtml += `</td>`
                 jours[jour].forEach(j => {
-                    document.write(`<td>`);
-                        document.write(`
-                            ${j.dt_txt.substr(11,5).split(':')[0]}h
-                            <br>
-                            ${+j.main.temp}°C
-                            <br>
+                    textHtml += `
+                        <td>
+                            ${j.dt_txt.substr(11,5).split(':')[0]}h 
                             <img src="https://openweathermap.org/img/wn/${j.weather[0].icon}@2x.png">
-                            `);
-                    document.write(`</td>`);
-                });
-            document.write(`</tr>`);
+                            ${+j.main.temp}°C
+                        </td>`
+                })
+            textHtml += `</tr>`
         }
-        document.write(`</tbody>`);
-    document.write(`</table>`);
-    console.table(data)
+        textHtml += `</tbody>`
+    textHtml += `</table>`
+    
+    container.innerHTML = textHtml;
 });
